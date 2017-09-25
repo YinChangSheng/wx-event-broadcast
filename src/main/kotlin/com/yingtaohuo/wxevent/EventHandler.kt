eccf78a65edee681d59f9e2af55ffe91ce427199
yinchangsheng <ychangsheng@gmail.com>
2017-09-26 07:37:38 +0800

    取出资源关闭代码

--- src/main/kotlin/com/yingtaohuo/wxevent/EventHandler.kt
+++ src/main/kotlin/com/yingtaohuo/wxevent/EventHandler.kt
@@ -68,7 +68,6 @@ class EventHandler(
         channel.basicPublish("wxtoken", "component_token", null, tokenText.toByteArray())
         // 存储到缓存
         redis.resource.set("component_token:${event.appId}", tokenText)
-        redis.resource.close()
     }
 
 }
