adad860979b513470d89919904871264e73bbf33
yinchangsheng <ychangsheng@gmail.com>
2018-03-26 17:53:17 +0800

    移除配置

--- src/main/kotlin/com/yingtaohuo/wxevent/Config.kt
+++ src/main/kotlin/com/yingtaohuo/wxevent/Config.kt
@@ -10,15 +10,15 @@ object Config {
     const val Port = 9002
 
     // 第三方平台相关
-    const val WX3rdAppId = "wxb3d033d520d15fe7"
-    const val WX3rdAppKey = "gh_e9100e798b39"
-    const val WX3rdAppToken = "dg156c5719d3202f32a6619e14D0ccqd"
-    const val WX3rdAppSecret = "99454ebe9eb76b704d9dfd8c34cc310b"
-    const val WX3rdEncodingAesKey = "kat52d5719d320nEj2A6l19u14H21ct2aI8K08rltKl"
+    const val WX3rdAppId = ""
+    const val WX3rdAppKey = ""
+    const val WX3rdAppToken = ""
+    const val WX3rdAppSecret = ""
+    const val WX3rdEncodingAesKey = ""
 
-    const val BroadcastRabbitMQUrl = "amqp://ythrabbit:28181820@yingtaohuo.com:5698/yth"
+    const val BroadcastRabbitMQUrl = ""
 
     // token 存储
-    const val redisHost = "redis://:4cj34t4j9@yingtaohuo.com:7389/9"
+    const val redisHost = ""
 
 }
